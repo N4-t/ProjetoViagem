7a7f6312d2d4fbcd5867840042b8429ece78be31
Natanael <natanaelsouza2005@hotmail.com>
2023-06-23 17:57:15 -0300

    projetoViagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ListagemViagensComponent } from './listagem-viagens/listagem-viagens.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovaViagemComponent } from './nova-viagem/nova-viagem.component';
+import { ExcluirViagemComponent } from './excluir-viagem/excluir-viagem.component';
+import { DetalhesViagemComponent } from './detalhes-viagem/detalhes-viagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'lista-viagens', component:ListagemViagensComponent},
+  {path:'cadastra-viagem', component:NovaViagemComponent},
+  {path:'excluir-viagem', component:ExcluirViagemComponent},
+  {path:'detalhes-viagem', component:DetalhesViagemComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
